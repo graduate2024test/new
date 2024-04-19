56d338f33148e390f45c0bced0bee48175a0bf7f
graduate2024test <graduate2024test@gmail.com>
2024-04-19 09:43:47 +0400

    add room.ejs + modify map.ejs

--- index.js
+++ index.js
@@ -4,27 +4,37 @@ const app = express()
 const PORT = 3000
 
 
+
 /* ********************************************* */
 /* *********************************** FUNCTIONS */
-const log_render_msg = function(req, res, next) {
-  console.log(`render map.html`)
-  next()
-}
-
-const res_render_ejs = function(req, res, next) {
-  res.render(`map.ejs`)
-}
-
-const log_port_msg = function(){
+const log_port_msg = function(req, res, next){
   console.log(`heard on port: ${PORT}`)
 }
 
+const log_route_msg = function(req, res, next) {
+  console.log(req.route.path)
+  next()
+}
+
+const get_map_ejs = function(req, res, next) {
+  res.render(`map.ejs`)
+}
+
+const get_room_ejs = function(req, res, next) {
+  var room_id_param = req.params['room_id']
+  console.log(`room id: ${room_id_param}`)
+  res.render(`room.ejs`)
+}
+
+
+
+/* ********************************************* */
+/* *********************************** CONSTANTS */
 const dirname_views = path.join(__dirname, `views`)
 
 const dirname_public = path.join(__dirname, `public`)
 
 const static_dir_public = express.static(dirname_public)
-
 
 
 
@@ -35,6 +45,18 @@ app.set('view engine', 'ejs')
 
 app.use( static_dir_public )
 
-app.get('/', log_render_msg, res_render_ejs )
+app.get('/', log_route_msg, get_map_ejs )
+
+app.get('/map', log_route_msg, get_map_ejs )
+
+app.get('/room/:room_id', log_route_msg, get_room_ejs )
+
+app.get('/timetable', log_route_msg )
 
 app.listen(PORT, log_port_msg )
+
+
+
+
+
+
