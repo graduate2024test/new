136e350c40986829f70770ae06409de53253e3e8
graduate2024test <graduate2024test@gmail.com>
2024-04-23 17:29:07 +0400

    concat(str1, str2)

--- index.js
+++ index.js
@@ -35,7 +35,9 @@ const get_room_ejs = function(req, res, next) {
   var room_id_param = req.params['room_id']
   console.log(`room id: ${room_id_param}`)
   
-  const jsonPath = path.join(process.cwd(), 'public', 'r310.json')   /* + */
+  const id_room_str = concat(room_id_param, '.json')
+  
+  const jsonPath = path.join(process.cwd(), 'public', id_room_str)   /* + */
   
   const scheduleList = JSON.parse(
   
